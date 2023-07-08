2aef32274edb918925771e9feb3ea3b4874f36ad
BrantleeMercer <98775028+BrantleeMercer@users.noreply.github.com>
2023-07-08 00:30:36 -0400

    Squashed commit of the following:
    commit 227e7ea7d7858a05ab19704a166f8e0ed4009858
    Author: BrantleeMercer <98775028+BrantleeMercer@users.noreply.github.com>
    Date:   Sat Jul 8 00:30:14 2023 -0400
    
        Adds basic layout for portfolio

---
+++ portfolio/src/Components/CustomHeader.js
@@ -0,0 +1,34 @@
+﻿import { useState, useEffect} from "react";
+import '../CSS Files/CustomHeaderOverrides.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { ReactComponent as GitHubLogo } from "../Saved-SVG-Files/GitHubLogo.svg";
+import { ReactComponent as LinkedInLogo } from "../Saved-SVG-Files/LinkedInLogo.svg";
+import { ReactComponent as EmailLogo } from "../Saved-SVG-Files/envelope-solid.svg";
+
+function CustomHeader() {
+    return (
+        <div className={"Custom-Header-Main"}>
+            <div className={"Name-Section"}>
+                <h2>Brantlee Mercer</h2>
+            </div>
+            <div className={"Resume-Section"}>
+                <button type={"button"} className={"btn btn-secondary"}>Resume</button>
+            </div>
+            <div className={"Links-Section"}>
+                <button type={"button"} className={"btn"}>
+                    <GitHubLogo />
+                </button>
+                <button type={"button"} className={"btn"}>
+                    <LinkedInLogo />
+                </button>
+                <button type={"button"} className={"btn"}>
+                    <EmailLogo />
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default CustomHeader;
---
+++ portfolio/src/Components/InfoContainer.js
@@ -0,0 +1,26 @@
+﻿import {useState, useEffect} from "react";
+import '../CSS Files/InfoContainerOverrides.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBowlingBall} from "@fortawesome/free-solid-svg-icons";
+
+function InfoContainer() {
+    return (
+        <div className={"Info-Container"}>
+            <div className={"Images"}>
+                
+            </div>
+            <div className={"Synopsis"}>
+                <h2>Course Title</h2>
+                This course was all about learning the Standard, Universal, and High Definition rendering pipelines in Unity!  Using the HDRP I created an old time feeling lawyers office that has the hints of a murder mystery painted on the walls.
+                <div className={"Completion"}>
+                    <h4>Completed On: </h4>
+                    <button type={"button"} className={"btn btn-success"}>Go To Project</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default InfoContainer;
