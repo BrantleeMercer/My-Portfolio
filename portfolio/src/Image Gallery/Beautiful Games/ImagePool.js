b666350ce2848e39e4ec9b98081b00e79334a976
BrantleeMercer <98775028+BrantleeMercer@users.noreply.github.com>
2023-10-18 16:54:13 -0400

    Revert "Merge branch 'gh-pages' of https://github.com/BrantleeMercer/My-Portfolio into gh-pages"
    This reverts commit 8ce1d001bbf5f0c3fd0331f11a492a7acc15d0af, reversing
    changes made to d4e171971ef7f2a3c56d8b4dc4fb715fb9d4d061.

---
+++ portfolio/src/Image Gallery/Beautiful Games/ImagePool.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import image1 from './Screenshot 1.jpg';
+import image2 from './Screenshot 2.png';
+import image3 from './Screenshot 3.jpg';
+import image4 from './Screenshot 4.jpg';
+
+const imagesPool = [
+    { src: image1},
+    { src: image2},
+    { src: image3},
+    { src: image4},
+];
+
+export default imagesPool;
---
+++ portfolio/src/Image Gallery/GLI 1/ImagePool.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import image1 from './Screenshot 1.png';
+import image2 from './Screenshot 2.png';
+import image3 from './Screenshot 3.png';
+import image4 from './Screenshot 4.png';
+import image5 from './Screenshot 5.png';
+
+const imagesPool = [
+    { src: image1},
+    { src: image2},
+    { src: image3},
+    { src: image4},
+    { src: image5},
+];
+
+export default imagesPool;
