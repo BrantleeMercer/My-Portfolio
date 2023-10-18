35b9cf0fd32e8bafcbdd41fa02741863df4c458d
BrantleeMercer <98775028+BrantleeMercer@users.noreply.github.com>
2023-10-18 16:43:24 -0400

    Revert "Updates"
    This reverts commit 46bb1d675f388ab62f587cad0f2b07f2f789321c.

---
+++ portfolio/src/Components/InfoContainer.js
@@ -0,0 +1,35 @@
+﻿import React from "react";
+import '../CSS Files/InfoContainerOverrides.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Carousel from "./Carousel";
+
+export default function InfoContainer(props) {
+    
+    const handleClick = (dest) => {
+        window.open(dest);
+    }
+    
+    return (
+        <div className={"Info-Container"}>
+            <div className={"Images"}>
+                <Carousel>
+                    {props.courseImageFolder.map((imgSrc, index) => 
+                        (<img src={imgSrc.src} key={index} alt="image"/>)
+                    )}
+                </Carousel>
+            </div>
+            <div className={"Synopsis"}>
+                <h2>{props.courseTitle}</h2>
+                <p>{props.courseDescription}</p>
+                <div className={"Completion"}>
+                    <h4>Completed On: {props.dateCompleted}</h4>
+                    <button onClick={() => {handleClick(props.destinationLinkURL)}} 
+                            className={"btn btn-success"}>
+                        Go To Project
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
